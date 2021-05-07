fa7e03159afda4031d4528d12e8ff8607c7f54b2
Leonardo Meza <52134252+leonardomeza87@users.noreply.github.com>
2021-05-07 00:26:55 -0400

    Revert "Form submission"
    This reverts commit 6a5b2d6ccc9e570bc43caf5e0f26d0ede6360e19.

--- js/main.js
+++ js/main.js
@@ -1,67 +1,10 @@
 const d = document,
-  $inputs = d.querySelectorAll(".input"),
-  $labels = d.querySelectorAll("label");
+  $inputs = d.querySelectorAll("input");
 
-console.log($inputs);
-
-for (let i = 0; i < $inputs.length; i++) {
-  if ($inputs[i].value) {
-    $labels[i].classList.add("selected");
-  }
-}
-
-const focus = (num) => {
-  for (let i = 0; i < $labels.length; i++) {
-    if ($labels[i].dataset.label === num) {
-      $labels[i].classList.add("selected");
-    } else {
-      if ($inputs[i].value === "") {
-        $labels[i].classList.remove("selected");
-      }
+d.addEventListener("", () => {
+  for (let i = 0; i < $inputs.length; i++) {
+    if ($inputs[i] === d.activeElement) {
+      console.log("a");
     }
   }
-};
-
-for (let i = 0; i < $inputs.length; i++) {
-  $inputs[i].addEventListener("focus", (e) => {
-    let dataInput = $inputs[i].dataset.input;
-    focus(dataInput);
-  });
-  $inputs[i].addEventListener("focusout", (e) => {
-    focus(-1);
-  });
-}
-
-d.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const $loader = d.querySelector(".loader"),
-    $pop = d.querySelector(".pop"),
-    $form = d.querySelector("form"),
-    $response = d.querySelector(".response");
-
-  $pop.classList.remove("none");
-
-  fetch("https://formsubmit.co/ajax/leonardomeza227@gmail.com", {
-    method: "POST",
-    body: new FormData(e.target),
-  })
-    .then((response) =>
-      response.ok ? response.json() : Promise.reject(response)
-    )
-    .then((json) => {
-      console.log(json);
-      $loader.classList.add("none");
-      $response.innerHTML = `<p>${json.message}</p>`;
-      $form.reset();
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-    .finally(() => {
-      setTimeout(() => {
-        $pop.classList.add("none");
-        $response.innerHTML = "";
-      }, 6000);
-    });
 });
